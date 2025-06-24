acdc677bd6313a3287c95b5515786f53f5635ab5
seungwon0113 <jungseungwon0113@gmail.com>
2025-06-24 22:46:27 +0900

    test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+from django.contrib import admin, messages
+
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "user", "created_at", "updated_at")
+    list_filter = ("category", "created_at")
+    search_fields = ("title", "content", "user__username", "user__nickname")
+    readonly_fields = ("created_at", "updated_at")
+    actions = ["reset_rendered_content"]
+
+    def reset_rendered_content(self, request, queryset):
+        for post in queryset:
+            post.rendered_content = ""
+            post.save(update_fields=["rendered_content"])
+
+        count = queryset.count()
+        messages.success(request, f"{count}개의 게시글 렌더링 내용이 초기화되었습니다.")
+
+    reset_rendered_content.short_description = "선택된 게시글의 렌더링된 내용 초기화"
